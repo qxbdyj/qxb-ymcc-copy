edca7d3993178818fac5bd68bf6f9c41036a8769
Qxb <1825218227@qq.com>
2022-09-06 17:02:53 +0800

    项目搭建2

---
+++ ymcc-service/ymcc-service-gateway/src/main/java/cn/qxb/ymcc/GateWayStarter.java
@@ -0,0 +1,17 @@
+package cn.qxb.ymcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: qxb-ymcc02
+ * @author: Qxb
+ * @date: 2022/9/6 15:01
+ * @description:
+ */
+@SpringBootApplication
+public class GateWayStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayStarter.class,args);
+    }
+}
